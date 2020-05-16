03bf2d623636d82ad03aec75cb9944f9fcd7d5f1
antogkou <mythoc@gmail.com>
2020-05-17 00:23:41 +0300

    First Commit #backup

---
+++ FundRaiser/Models/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FundRaiserMVC.Models
+{
+    public class AppUser : IdentityUser<int>
+    {
+        public string FirstName { get; set; } 
+        public string LastName { get; set; }
+    
+    }
+}
---
+++ FundRaiser/Models/MVSJwtTokens.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+namespace FundRaiserMVC.Models
+{
+    public class MVSJwtTokens
+    {
+        public const string Issuer = "MVS";
+        public const string Audience = "ApiUser";
+        public const string Key = "1234567890123456";
+
+        public const string AuthSchemes =
+            "Identity.Application" + "," + JwtBearerDefaults.AuthenticationScheme;
+    }
+}
