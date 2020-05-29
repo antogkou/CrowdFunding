b4e64a26c57e4ffed27a99222747046191e8ac51
Nikos Pavlidis <nikospavlidismail@gmail.com>
2020-05-29 19:54:03 +0300

    Fixed redirections by changing [HttpGet] from project controller, added viewmodels to support it.

---
+++ CrowdFundingMVC/ViewModels/EditPledgeVM.cs
@@ -0,0 +1,14 @@
+﻿using CrowdFundingCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrowdFundingMVC.ViewModels
+{
+    public class EditPledgeVM
+    {
+        public Project Project { get; set; }
+        public Pledge Pledge { get; set; }
+    }
+}
